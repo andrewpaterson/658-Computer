dda2969eefe595c5eb5ed4d553553fa10fb4942a
Andrew Paterson <andrew.ian.paterson@gmail.com>
2021-09-14 07:23:13 +0200

    Added Logi6502 converted to run in Logisim Evolution.

---
+++ logi6502/src/name/bizna/logi6502/Components.java
@@ -0,0 +1,48 @@
+package name.bizna.logi6502;
+
+import com.cburch.logisim.tools.AddTool;
+import com.cburch.logisim.tools.Library;
+import com.cburch.logisim.tools.Tool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Components
+    extends Library
+{
+  private final ArrayList<Tool> tools;
+
+  public Components()
+  {
+    try
+    {
+      tools = new ArrayList<Tool>();
+      tools.add(new AddTool(new Logi6502Simple()));
+      tools.add(new AddTool(new Logi6502Expert()));
+      tools.add(new AddTool(new Logi6502_PDIP40()));
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      throw e;
+    }
+  }
+
+  @Override
+  public String getName()
+  {
+    return "W65C02S";
+  }
+
+  @Override
+  public boolean removeLibrary(String name)
+  {
+    return false;
+  }
+
+  @Override
+  public List<? extends Tool> getTools()
+  {
+    return tools;
+  }
+}

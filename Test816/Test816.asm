dc517f1d5c8f3c26549fda09c83a1f5d869d4d93
Andrew Paterson <andrew.ian.paterson@gmail.com>
2021-09-24 14:38:18 +0200

    Simple Test to ensure the Java 65816 emulator does *something* at least.

---
+++ Test816/Test816.asm
@@ -0,0 +1,68 @@
+STARTUP SECTION
+
+	ORG	$0000
+
+	DB '['
+PROGRESS:	
+	DB 'xxxxxxxxxxxxxxxx'
+ENDPROGRESS:	
+	DB ']'
+	
+	ORG $F000
+	
+START:
+	CLC	     		;clear carry
+	XCE	     		;clear emulation
+	REP		#$30    ;16 bit registers
+	LONGI 	ON
+	LONGA	ON
+	LDA		#$0100  ;get the stack address
+	TCS	     		;and set the stack to it
+
+
+	SEP		#$20	;8 bit accum
+	LONGA 	OFF
+	LDA 	#$00	;get bank of data
+	PHA
+	PLB	     		;set data bank register
+	REP		#$20    ;back to 16 bit mode
+	LONGA	ON
+
+	JMP   MYSTART	;long jump in case not bank 0
+
+	ORG $0400
+
+MYSTART
+	SEP		#$30	;8 bit registers
+	LONGA 	OFF
+	LONGI 	OFF
+
+	LDA #$2e
+	LDY #PROGRESS
+LOOP:	
+	STA 0,Y
+	INY
+	CPY #ENDPROGRESS
+	BNE LOOP
+	STP
+	
+; This section defines the interrupt and reset vectors.    
+
+	ORG	$FFE4
+
+N_COP   DW    0
+N_BRK   DW    0
+N_ABORT DW    0
+N_NMI   DW    0
+N_RSRVD DW    0
+N_IRQ   DW    0
+	DS    4
+E_COP   DW    0
+E_RSRVD DW    0
+E_ABORT DW    0
+E_NMI   DW    0
+E_RESET DW    START
+E_IRQ   DW    0
+
+	ENDS
+	END

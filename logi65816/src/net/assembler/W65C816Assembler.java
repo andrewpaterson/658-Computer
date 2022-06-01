c5fa06812d653b112f6eff61603e8dab075dfded
Andrew Paterson <andrew.ian.paterson@gmail.com>
2022-06-01 11:39:20 +0200

    Test file for Java parser.

--- logi65816/src/net/assembler/W65C816Assembler.java
+++ logi65816/src/net/assembler/W65C816Assembler.java
@@ -1,22 +1,138 @@
 package net.assembler;
 
 import static net.simulation.common.TraceValue.*;
+
 import net.simulation.common.TraceValue;
+import net.util.StringUtil;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public class W65C816Assembler<X, Y extends List<Map<X, ? extends Integer>>>
+public strictfp abstract class W65C816Assembler<X, Y extends List<Map<X, ? extends Integer[]>>>
 {
   private int y;
   private X x;
+  public static final String s = "";
+  java.util.Date date = new Date();
+  protected int[][] ia = {{3}, {(int) multiply(4, 4), 5}, {0, 1, 3, (int) multiply(2, 2)}};
+  private java.lang.String sa[] = {"1", "2"};
+  public final List<Map<? extends X, ? extends Integer>> finalMap = new ArrayList<>();
+
+  static final protected float z = multiply(Sinternal.InterfaceInAClassInAClass.InnerFace.LetOut(), 2);
+
+  {
+    final int x = (int) multiply(4, 3);
+    this.y = x;
+
+    class CodeBlock
+    {
+      void whatDoesThisEvenMean()
+      {
+      }
+    }
+    CodeBlock codeBlock = new CodeBlock();
+    codeBlock.whatDoesThisEvenMean();
+  }
 
   public W65C816Assembler()
   {
-    TraceValue value = NotConnected;
+    final TraceValue value = NotConnected;
     List<Map<? extends X, ? extends Integer>> map = new ArrayList<>();
     int x = 5;
+    String sb[][] = new String[8][(int) multiply(3, 2)];
+    sb[0][0] = "";
+    List<X> xt = (List<X>)new ArrayList<>();
+  }
+
+  private enum Jeb
+  {
+  }
+
+  void DoStuff(int x)
+  {
+    x--;
+  }
+
+  private class Internal
+  {
+    {
+      int x = 3;
+      W65C816Assembler.this.y = x;
+    }
+
+    public Internal()
+    {
+    }
+  }
+
+  private static final class Sinternal
+  {
+    static
+    {
+      int x = 3;
+    }
+
+    enum Seriously
+    {
+      For,
+      Realz;
+
+      public String toEnumString()
+      {
+        return StringUtil.toEnumString(this);
+      }
+    }
+
+    interface InterfaceInAClassInAClass
+    {
+      enum InnNume
+      {
+        Sons,
+        Of,
+        Cornhole;
+      }
+
+      class InnerFace
+      {
+        int y;
+
+        public InnerFace(int y)
+        {
+          this.y = y;
+        }
+
+        public static final float LetOut()
+        {
+          return 5;
+        };
+      };
+    };
+
+    public Sinternal()
+    {
+    }
+
+    void Internaltuff(int x)
+    {
+    }
+  }
+
+  static float multiply(float a, float b)
+  {
+    return a * b;
+  }
+}
+
+strictfp enum Loki
+{
+  Glorb,
+  Mork;
+
+  public String toEnumString()
+  {
+    return StringUtil.toEnumString(this);
   }
 }
 

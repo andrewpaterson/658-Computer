17c8fceee5d8171dee85214163c0d7adad8d9cf2
Andrew Paterson <andrew.ian.paterson@gmail.com>
2023-03-02 08:29:37 +0200

    Fixed subcircuit data saving.

--- Logicim/src/net/logicim/data/circuit/SubcircuitData.java
+++ Logicim/src/net/logicim/data/circuit/SubcircuitData.java
@@ -1,11 +1,13 @@
 package net.logicim.data.circuit;
 
+import net.logicim.data.ReflectiveData;
 import net.logicim.data.integratedcircuit.common.StaticData;
 import net.logicim.data.wire.TraceData;
 
 import java.util.List;
 
 public class SubcircuitData
+    extends ReflectiveData
 {
   public List<StaticData<?>> components;
   public List<TraceData> traces;

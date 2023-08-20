38e43d57fc3f2a379bf4f44543be0209f4ea659c
Andrew Paterson <andrew.ian.paterson@gmail.com>
2023-08-20 08:47:15 +0200

    Experimenting with the WDC C compiler.

--- WDC/main.cpp
+++ WDC/main.cpp
@@ -1,8 +1,37 @@
+char getChar(char c, char* sz, int iLength)
+{
+	int i;
+	for (i = 0; i < iLength; i++)
+	{
+		if (sz[i] == '\0')
+		{
+			return 0xff;
+		}
+
+		if (sz[i] == c)
+		{
+			return sz[i];
+		}
+	}
+	return 0xff;
+}
+
+
+char gasz[6];
+
 void main(void)
 {
-	int x = 3;
-	for (int y = 0; i < 4; i++)
+	int i;
+	gasz[0] = 'H';
+	gasz[1] = 'e';
+	gasz[2] = 'l';
+	gasz[3] = 'l';
+	gasz[4] = 'o';
+	gasz[5] = '\0';
+
+	for (i = 0; i < 4; i++)
 	{
-		x = x + y;
+		getChar('A' + i, gasz, 5);
 	}
 }
+

6400b96336debc84197744541c62dab1afc83d77
Andrew Paterson <andrew.ian.paterson@gmail.com>
2024-07-27 15:58:27 +0200

    I messed up.

--- Logicim/src/net/assembler/sixteenhigh/tokeniser/SixteenHighKeywordCode.java
+++ Logicim/src/net/assembler/sixteenhigh/tokeniser/SixteenHighKeywordCode.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package net.assembler.sixteenhigh.tokeniser;
 
 public enum SixteenHighKeywordCode
@@ -66,3 +67,73 @@ public enum SixteenHighKeywordCode
   UNKNOWN
 }
 
+=======
+package net.assembler.sixteenhigh.tokeniser;
+
+public enum SixteenHighKeywordCode
+{
+  int8,
+  uint8,
+  int16,
+  uint16,
+  int32,
+  uint32,
+  int64,
+  uint64,
+  float16,
+  float32,
+  float64,
+  float128,
+  flag,
+  bool,
+  assign,             // =
+  add,                // +
+  subtract,           // -
+  multiply,           // *
+  divide,             // /
+  modulus,            // %
+  shift_left,         // <<
+  shift_right,        // >>
+  ushift_right,       // >>>
+  and,                // &
+  or,                 // |
+  xor,                // ^
+  not,                // ~
+  if_,                // if
+  add_assign,         // +=
+  subtract_assign,    // -=
+  multiply_assign,    // *=
+  divide_assign,      // /=
+  modulus_assign,     // %=
+  shift_left_assign,  // <<=
+  shift_right_assign, // >>=
+  ushift_right_assign,// >>>=
+  and_assign,         // &=
+  or_assign,          // |=
+  xor_assign,         // ^=
+  not_assign,         // ~=
+  increment,          // ++
+  decrement,          // --
+
+  test_set,           // ts
+  test_reset,         // tr
+  return_,            // return
+  sub,                // sub
+  rec,                // rec
+  end,
+  start_address,
+  end_address,
+  access_mode,
+  read_only,
+  write_only,
+  read_write,
+  access_time,
+  open_round,         // (
+  close_round,        // )
+  open_square,        // [
+  close_square,       // ]
+
+  UNKNOWN
+}
+
+>>>>>>> Stashed changes
--- Logicim/src/net/assembler/sixteenhigh/tokeniser/statment/BitCompareTokenStatement.java
+++ Logicim/src/net/assembler/sixteenhigh/tokeniser/statment/BitCompareTokenStatement.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package net.assembler.sixteenhigh.tokeniser.statment;
 
 import net.assembler.sixteenhigh.common.SixteenHighKeywords;
@@ -17,3 +18,24 @@ public class BitCompareTokenStatement
   }
 }
 
+=======
+package net.assembler.sixteenhigh.tokeniser.statment;
+
+import net.assembler.sixteenhigh.common.SixteenHighKeywords;
+import net.assembler.sixteenhigh.tokeniser.SixteenHighKeywordCode;
+import net.assembler.sixteenhigh.tokeniser.statment.expression.VariableTokenExpression;
+
+public class BitCompareTokenStatement
+    extends TokenStatement
+{
+  protected VariableTokenExpression variableExpression;
+  protected SixteenHighKeywordCode keyword;
+
+  @Override
+  public String print(SixteenHighKeywords sixteenHighKeywords)
+  {
+    return variableExpression.print(sixteenHighKeywords) + " " + sixteenHighKeywords.getKeyword(keyword) + semicolon();
+  }
+}
+
+>>>>>>> Stashed changes
